080289c241d4700a6720f051585f201b602181d3
Kelly L <lock2846@gmail.com>
2018-09-20 19:59:39 -0600

    simple styling

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './FeedItem.css'
+import Grid from '@material-ui/core/Grid'
 
 const FeedItem = ({item}) => {
-  return (<div>
-    item={item}
-  </div>
-})
+  return (
+    <div>
+      <Grid>
+        <div className='handle'>{item.handle}</div>
+        <div className='text'>{item.text}</div>
+      </Grid>
+    </div>
+  )
+}
 
 export default FeedItem
